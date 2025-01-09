abfd95e4703ad3366b0d53efecbf3ebf33900a9a
Trevor Mustoe <trevormustoe0405@gmail.com>
2025-01-09 11:45:15 -0500

    Created a second branch that took out the url and added in some notes

--- HTMLGRABBER.py
+++ HTMLGRABBER.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.devonbristolshaw.com/"
+# add in the url that you woudl like to scrape to the url below
+url = ""
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
 response = requests.get(url, headers=headers)
